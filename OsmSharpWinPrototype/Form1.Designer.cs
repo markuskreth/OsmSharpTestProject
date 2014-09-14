3732e5a3cb55a35cf24108eb35a14719f400ca3e
Markus Kreth <markus.kreth@web.de>
2014-09-14 21:30:26 +0200

    Working WinForm Project importing OsmData into Sql Server Database

---
+++ OsmSharpWinPrototype/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace OsmSharpWinPrototype
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelImportCount = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(3, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(754, 427);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(672, 448);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Import Data";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 448);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Imported:";
+            // 
+            // labelImportCount
+            // 
+            this.labelImportCount.AutoSize = true;
+            this.labelImportCount.Location = new System.Drawing.Point(61, 447);
+            this.labelImportCount.Name = "labelImportCount";
+            this.labelImportCount.Size = new System.Drawing.Size(0, 13);
+            this.labelImportCount.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(591, 448);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Cancel";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(759, 483);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelImportCount);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelImportCount;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ OsmSharpWinPrototype/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using OsmSharp.Osm.Streams.Complete;
+using OsmSharp.Osm;
+using OsmSharp.Osm.Data.Memory;
+using OsmSharp.UI.Map.Styles.MapCSS;
+using OsmSharp.Data.SQLServer.Osm.Streams;
+using OsmSharp.Osm.Xml.Streams;
+using OsmSharp.Osm.Streams;
+using System.Threading;
+using OsmSharp.WinForms.UI;
+using OsmSharp.Data.SQLServer.Osm;
+
+namespace OsmSharpWinPrototype
+{
+    public partial class Form1 : Form
+    {
+        private ImportToDbWorker importWorker = null;
+        private Thread importThread = null;
+        private System.Threading.Timer tim;
+        private SQLServerDataSource sqlSource;
+        private System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+
+        public Form1()
+        {
+            InitializeComponent();
+            MapControl mapcon = new MapControl();
+            sqlSource = new SQLServerDataSource("Server=VIRTUALBOX;Database=osm;User Id=sa; Password=0773", false);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog(this);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            if (tim != null)
+            {
+                tim.Dispose();
+            }
+
+            importWorker = new ImportToDbWorker(openFileDialog1.FileName, this.stopTimeOfWorker);
+            importThread = new Thread(importWorker.doWork);
+            importThread.Name = importWorker.GetType().Name;
+            importThread.Start();
+
+            TimerCallback callback = this.updateStatus;
+            tim = new System.Threading.Timer(callback, null, 500, 100);
+
+        }
+
+        private void stopTimeOfWorker(bool workerRunning)
+        {
+            if (workerRunning)
+            {
+                stopWatch.Restart();
+
+                button2.BeginInvoke(new Action(() => button2.Enabled = false));
+            }
+            else
+            {
+                TimeSpan span = stopWatch.Elapsed;
+                if (tim != null)
+                {
+                    tim.Dispose();
+                    tim = null;
+                }
+                try
+                {
+                    button2.BeginInvoke(new Action(() => button2.Enabled = true));
+                    labelImportCount.BeginInvoke(new Action(() => labelImportCount.Text = labelImportCount.Text + " - Dauer: " + span.ToString()));
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex);
+                }
+
+            }
+        }
+
+        private void updateStatus(Object obj)
+        {
+
+            try
+            {
+                StringBuilder text = new StringBuilder();
+                foreach (KeyValuePair<OsmGeoType, Int32> current in importWorker.typeCount)
+                {
+                    if (text.Length > 0)
+                        text.Append("; ");
+                    text.Append(current.Key).Append("s=").Append(current.Value);
+
+                }
+
+                TimeSpan span = stopWatch.Elapsed;
+                text.Append(" - Zeit benötigt: ").Append(span.ToString());
+
+                labelImportCount.BeginInvoke(new Action(() => labelImportCount.Text = text.ToString()));
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
+        }
+
+        public class ImportToDbWorker
+        {
+            public delegate void WorkerIsRunning(bool running);
+
+            private String filename;
+            public Dictionary<OsmGeoType, Int32> typeCount = new Dictionary<OsmGeoType, int>();
+            private WorkerIsRunning handler;
+
+            public ImportToDbWorker(String filename, WorkerIsRunning handler)
+            {
+                this.filename = filename;
+                this.handler = handler;
+                _shouldStop = false;
+            }
+
+            public void doWork()
+            {
+                handler.Invoke(true);
+                SQLServerOsmStreamTarget sqlTarget = new SQLServerOsmStreamTarget("Server=VIRTUALBOX;Database=osm_test;User Id=sa; Password=0773", true);
+
+                sqlTarget.Initialize();
+                FileStream stream = File.OpenRead(filename);
+                OsmStreamSource src;
+
+                switch (Path.GetExtension(filename).ToLower())
+                {
+                    case ".pbf":
+                        src = new OsmSharp.Osm.PBF.Streams.PBFOsmStreamSource(stream);
+                        break;
+                    case ".osm":
+                    default:
+                        src = new XmlOsmStreamSource(stream);
+                        break;
+                }
+
+                if (!_shouldStop)
+                    transferData(src, sqlTarget);
+                handler.Invoke(false);
+
+            }
+
+            private void transferData(OsmStreamSource src, OsmStreamTarget sqlTarget)
+            {
+                src.Initialize();
+                sqlTarget.Initialize();
+
+                while (src.MoveNext(false, false, false) && !_shouldStop)
+                {
+
+                    OsmGeo sourceObject = src.Current();
+
+                    if (!typeCount.ContainsKey(sourceObject.Type))
+                    {
+                        typeCount.Add(sourceObject.Type, 0);
+                    }
+
+                    typeCount[sourceObject.Type] = 1 + typeCount[sourceObject.Type];
+
+                    if (sourceObject is Node)
+                    {
+                        sqlTarget.AddNode(sourceObject as Node);
+                    }
+                    else if (sourceObject is Way)
+                    {
+                        sqlTarget.AddWay(sourceObject as Way);
+                    }
+                    else if (sourceObject is Relation)
+                    {
+                        sqlTarget.AddRelation(sourceObject as Relation);
+                    }
+                }
+
+                try
+                {
+
+                    sqlTarget.Flush();
+                    sqlTarget.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex);
+                }
+            }
+
+            public void requestStop()
+            {
+                _shouldStop = true;
+            }
+
+            // Volatile is used as hint to the compiler that this data
+            // member will be accessed by multiple threads.
+            private volatile bool _shouldStop;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (importWorker != null)
+            {
+                importWorker.requestStop();
+                importWorker = null;
+                importThread.Interrupt();
+                importThread = null;
+            }
+            if (tim != null)
+            {
+                tim.Dispose();
+                tim = null;
+            }
+        }
+
+    }
+
+}
